7e9fba26d5d9432d76a4acd2c2e002cea08d0069
Jordan J <jordanr2m@gmail.com>
2024-03-07 14:57:07 -0800

    Added functionality to add a new note.

--- src/App.js
+++ src/App.js
@@ -6,32 +6,33 @@ class App extends Component {
   state = {
     notes: [
       {
-        id: 0,
-        title: "eat",
-        description: "reese peanut butter cups",
-        doesMatchSearch: true
+        id: Date.now(),
+        title: "",
+        description: "",
+        doesMatchSearch: true,
       },
-      {
-        id: 1,
-        title: "sleep",
-        description: "eight hours",
-        doesMatchSearch: true
-      },
-      {
-        id: 2,
-        title: "code",
-        description: "build an awesome ui",
-        doesMatchSearch: true
-      }
     ],
 
     searchText: "search for me",
   };
 
+  addNote = () => {
+    // create new note
+    const newNote = {
+      id: Date.now(),
+      title: "",
+      description: "",
+      doesMatchSearch: true,
+    };
+    // add new note to existing notes array in state
+    const newNotes = [newNote, ...this.state.notes]
+    this.setState({ notes: newNotes });
+  }
+
   render() {
     return (
       < div >
-        <Header searchText={this.state.searchText}/>
+        <Header searchText={this.state.searchText} addNote={this.addNote}/>
         <NotesList notes={this.state.notes}/>
       </div >
     );
