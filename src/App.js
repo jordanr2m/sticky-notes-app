55cde7b88b4fa4c50130db412eb75ddba89e0793
Jordan J <jordanr2m@gmail.com>
2024-03-18 11:59:06 -0700

    Added a prop for the App component to pass down to the NotesList component and then to the Notes component.

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ class App extends Component {
   render() {
     return (
       < div >
-        <Header searchText={this.state.searchText} addNote={this.addNote} />
+        <Header searchText={this.state.searchText} addNote={this.addNote} onSearch={this.onSearch}/>
         <NotesList notes={this.state.notes} onType={this.onType} removeNote={this.removeNote} />
       </div >
     );
